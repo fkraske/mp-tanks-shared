6ba74ad020f908fe745324bebe6c300ef20f35ae
Slihfer <49499110+Slihfer@users.noreply.github.com>
2022-10-06 16:49:39 +0200

    fix constants import

--- game/constants.ts
+++ game/constants.ts
@@ -1,9 +1,4 @@
 import { Vector2 } from "../framework/math/Vector2"
 
-export namespace Constants {
-  export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8080
-  export const TICK_RATE = 1
-  export const CHRONOLOGY_DURATION = 5
-
-  export const LEVEL_EXTENTS = Vector2.One
-}
+export const PORT = 8080
+export const LEVEL_EXTENTS = Vector2.One
