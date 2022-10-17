31cf458f435f036bdd3283290189791b778a79ab
Slihfer <49499110+Slihfer@users.noreply.github.com>
2022-10-17 20:05:07 +0200

    add domain constant

--- game/constants.ts
+++ game/constants.ts
@@ -1,6 +1,7 @@
 import { Vector2 } from '../framework/math/Vector2'
 
 export const SERVER_TICK_RATE = 1
+export const DOMAIN = 'localhost'
 export const STATIC_PORT = 8000
 export const SOCKET_IO_PORT = 8080
 export const LEVEL_EXTENTS = Vector2.One
